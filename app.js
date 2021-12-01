e3ff0b7a3fe6740062aa613662703cd3b4405e05
ATHE22 <95362668+ATHE22@users.noreply.github.com>
2021-12-01 15:45:31 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var num = Math.floor(Math.random() * 4) + 1;
 $("#card").wScratchPad({
-  size: 100, // The size of the brush/scratch.
+  size: 150, // The size of the brush/scratch.
   bg: `CARD${num}.jpg`, // Background (image path or hex color).
-  fg: `front.jpg`, // Foreground (image path or hex color).
+  fg: `FRONT.jpg`, // Foreground (image path or hex color).
   cursor: "pointer", // Set cursor.
 });
